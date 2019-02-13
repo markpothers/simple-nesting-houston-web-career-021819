134e94e2e4f401c346ce12bf1054558f8e80ed22
Mark Pothecary <markpothers@hotmail.com>
2019-02-13 15:11:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,7 +68,7 @@ def adding_matz
 # }
 
 	programmer_hash =
- 		{
+ 		{ 	
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -83,6 +83,10 @@ def adding_matz
         }
      }
 
+		 programmer_hash[:yukihiro_matsumoto] = {
+ 					:known_for => "Ruby",
+ 					:languages => ["LISP", "C"]						
+ 				}
 
 end
 
